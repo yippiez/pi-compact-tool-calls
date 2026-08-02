ed001f136d1c1c1024f4aa9210364e80958cef7a
Eren <erencemayez@gmail.com>
2026-08-02 10:55:45 +0300

    chore: use named Pi extension directory

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ pi install -l git:github.com/yippiez/pi-compact-tool-calls
 ```bash
 npm install
 npm run typecheck
-pi --no-extensions -e .
+pi --no-extensions -e ./extensions/pi-compact-tool-calls/index.ts
 ```
 
 Extracted from the current implementation in [pchain](https://github.com/yippiez/pchain).
--- extensions/pi-compact-tool-calls/index.ts
+++ extensions/pi-compact-tool-calls/index.ts
@@ -1,5 +1,5 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import { CompactToolCallsRegister } from "./feature/tool/compact_tool_calls.ts";
+import { CompactToolCallsRegister } from "../../src/feature/tool/compact_tool_calls.ts";
 
 export default function PiCompactToolCallsRegister(pi: ExtensionAPI): void {
 	CompactToolCallsRegister(pi);
